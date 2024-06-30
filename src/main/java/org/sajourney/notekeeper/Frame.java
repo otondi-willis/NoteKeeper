8c6f64e0cf11d7b69f052733a05bd43f68d8a8bb
otondi-willis <willis.otondi.ke@gmail.com>
2024-06-30 17:16:02 +0300

    Added text area to the GUI

--- src/main/java/org/sajourney/notekeeper/Frame.java
+++ src/main/java/org/sajourney/notekeeper/Frame.java
@@ -2,8 +2,10 @@ package org.sajourney.notekeeper;
 import javax.swing.*;
 public class Frame {
     public static JFrame frame;
+    public static JTextArea textArea;
     public static void main(String[] args){
         createFrame();
+        createTextarea();
         frame.setVisible(true);
     }
     public static void createFrame(){
@@ -11,6 +13,10 @@ public class Frame {
         frame.setSize(500, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+    public static void createTextarea(){
+        textArea = new JTextArea();
+        frame.add(textArea);
     }
 
 }
