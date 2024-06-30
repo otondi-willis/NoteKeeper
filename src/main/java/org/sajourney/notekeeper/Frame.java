b68687190b2b12c769ecd7bbf51f37c3c93f2d87
otondi-willis <willis.otondi.ke@gmail.com>
2024-06-30 17:23:39 +0300

    Added vertical scrollbar for the GUI

--- src/main/java/org/sajourney/notekeeper/Frame.java
+++ src/main/java/org/sajourney/notekeeper/Frame.java
@@ -10,13 +10,17 @@ public class Frame {
     }
     public static void createFrame(){
         frame = new JFrame("Note Keeper");
-        frame.setSize(500, 600);
+        frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
     public static void createTextarea(){
         textArea = new JTextArea();
         frame.add(textArea);
+
+        JScrollPane scrollbar = new JScrollPane(textArea);
+        scrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        frame.add(scrollbar);
     }
 
 }
